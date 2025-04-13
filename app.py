82e38a9d8fa75b6c4b43263716d39966d66395fa
nnamyslowska <104467124+nnamyslowska@users.noreply.github.com>
2025-04-13 23:02:28 +0000

    Implement user authentication with Flask-Login and add registration/login forms ! the dashboard is still not working

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SECRET_KEY'] = 'secret'
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for('login'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,17 +34,78 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(80), nullable=False)
 
 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    repeat_password = PasswordField('Repeat Password', validators=[InputRequired(), Length(min=4, max=80)])
+    email = StringField('Email', validators=[InputRequired(), Length(min=6, max=35)], render_kw={"placeholder": "Email"})
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user = User.query.filter_by(username=username.data).first()
+        if existing_user:
+            raise ValidationError("Username already exists. Please choose a different one.")
+    def validate_password(self, password):
+        if password.data != self.repeat_password.data:
+            raise ValidationError("Passwords do not match. Please try again.")
+        
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+    def validate_username(self, username):
+        existing_user = User.query.filter_by(username=username.data).first()
+        if not existing_user:
+            raise ValidationError("Username does not exist. Please register first.")
+    def validate_password(self, password):
+        existing_user = User.query.filter_by(username=self.username.data).first()
+        if existing_user and existing_user.password != password.data:
+            raise ValidationError("Incorrect password. Please try again.")
+
+
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])   
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+        else:
+            flash("Invalid credentials, please try again.", "danger")
+    return render_template("login.html", form=form)
 
-@app.route('/register')
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    if not current_user.is_authenticated:
+        flash("Please log in to access the dashboard.", "warning")
+        return redirect(url_for('login'))
+    return render_template("dashboard.html", name=current_user.username)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.", "info")
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template("register.html", form=form)
+
 
 if __name__ == '__main__':
     with app.app_context():
